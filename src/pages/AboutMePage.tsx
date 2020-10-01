57b65cdaf1a7de9a64fa46a142c83b75ae8dc389
Timmy Chan <timmychan301197@gmail.com>
2020-10-01 23:34:57 +0200

    Add about me content

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -7,6 +7,9 @@ export default function AboutMePage() {
       <div className="page-content-margined">
         <h1 className="page-title">About Me</h1>
         <Divider />
+        <p>
+          I am an engaging student who does a lot besides school. Long before I started my studies, I was a hobbyist in IT. I realized early on the possibilities a computer had, and what I could use it for. In middle school, I explored web development, .NET, Unity 3D and modeling all by myself. It was a challenging task, because there were few online learning opportunities at the time the technology was not as mature as it is today. I have a great curiosity and commitment to technology and programming, and have done many remarkable projects. I want to explore further in the areas I have never had the opportunity to sit down for.
+        </p>
       </div>
     </div>
   );
